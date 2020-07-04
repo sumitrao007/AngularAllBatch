bb708a58e45f67608bcc81dc953849b36cb32ecb
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2020-07-04 18:01:03 +0530

    Day2

---
+++ Typescript/Day2/demo1.ts
@@ -0,0 +1,48 @@
+console.log("Welcome To My Front End Session...");
+
+var a;//any
+var b_a:number;
+//data type any,number,string,boolean,undefined,null
+var c:string;
+var d:boolean;
+var a1:number=10;
+console.log("Value of A1 is "+a1);
+
+var a2:number;
+console.log("-------------------------------");
+console.log("Value is "+a2);
+var str1:string;
+console.log("-------------------------------");
+console.log("Value  Str1 is "+str1);
+console.log("---------------------");
+var a3;
+a3="Sumit Raokhande";
+console.log("Value of A3 is "+a3);
+a3=20;
+console.log("Value of A3 is "+a3);
+console.log("Current Data type of A3 is "+typeof a3);
+a3='Typescript...';
+console.log("Value of A3 is "+a3+" data type is "+typeof(a3) );
+
+var a4=20;
+//a4="dbvnmsd"
+
+//type assertion
+// variable should have any datatype
+var a5;
+//we can use type assertion in two ways
+//1 use angle bracket
+//2 use as syntax
+
+
+
+//1 Angle bracket
+//a5=20;
+//(<string> a5).
+
+//2 as syntax
+a5="sumit";
+
+ console .log((a5 as string).concat(" Raokhande"));
+
+
---
+++ Typescript/Day2/demo2.ts
@@ -0,0 +1,33 @@
+//airthematic,logical,bitwise,conditional,ternary,comparision.
+//increment,decrement,assignment
+//+,*,-,/....
+//a+=20;
+
+var a:number=10;
+var b:number=30;
+var res=a+b;
+console.log("Addition is "+res);
+var res1=a<9?"Hi":"Bye";
+console.log("Output of a is  "+res1);
+
+//Loop
+//if,if..else,nestated if else 
+var c:number=12;
+if(c<10){
+    console.log("Condition is true");
+}else
+{
+    console.log("Condition is false");
+
+}
+
+var c2:number=5;
+while(c2!=0){
+    console.log("Value of a is "+c2);
+    //c2=c2-1;
+    c2--;
+}
+console.log("vale od a out of while loop... "+c2 );
+
+
+
